a0247c8410f8d7188f66b164bfe952b90a00cc12
kbrStefan <kbrStefan@users.noreply.github.com>
2025-08-02 14:28:42 +0200

    added service and script for running the container as user arma without adding user arma to group docker (for security)

---
+++ run_arma_server.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export MY_UID=$(id -u)
+export MY_GID=$(id -g)
+
+echo "entering kranich folder"
+cd "$(dirname "$0")"
+
+case "$1" in
+  start)
+    echo "Starting container..."
+    docker compose up --build -d
+    ;;
+  stop)
+    echo "Stopping container..."
+    docker compose down
+    ;;
+  *)
+    echo "Usage: $0 {start|stop}"
+    exit 1
+    ;;
+esac
